3ae2a4f749c054a96b045bf5305f687b92249f38
TheMeanCanEHdian <drivard100@gmail.com>
2021-09-06 19:50:41 -0500

    Create new server entity and model.

---
+++ lib/core_new/database/data/models/new_server_model.dart
@@ -0,0 +1,185 @@
+import 'package:validators/sanitizers.dart';
+
+import '../../domain/entities/new_server.dart';
+
+class NewServerModel extends NewServer {
+  NewServerModel({
+    int? id,
+    int? sortIndex,
+    required String plexName,
+    required String plexIdentifier,
+    required String tautulliId,
+    required String primaryConnectionAddress,
+    required String primaryConnectionProtocol,
+    required String primaryConnectionDomain,
+    String? primaryConnectionPath,
+    String? secondaryConnectionAddress,
+    String? secondaryConnectionProtocol,
+    String? secondaryConnectionDomain,
+    String? secondaryConnectionPath,
+    required String deviceToken,
+    required bool primaryActive,
+    required bool onesignalRegistered,
+    required bool plexPass,
+    String? dateFormat,
+    String? timeFormat,
+  }) : super(
+          id: id,
+          sortIndex: sortIndex,
+          plexName: plexName,
+          plexIdentifier: plexIdentifier,
+          tautulliId: tautulliId,
+          primaryConnectionAddress: primaryConnectionAddress,
+          primaryConnectionProtocol: primaryConnectionProtocol,
+          primaryConnectionDomain: primaryConnectionDomain,
+          primaryConnectionPath: primaryConnectionPath,
+          secondaryConnectionAddress: secondaryConnectionAddress,
+          secondaryConnectionProtocol: secondaryConnectionProtocol,
+          secondaryConnectionDomain: secondaryConnectionDomain,
+          secondaryConnectionPath: secondaryConnectionPath,
+          deviceToken: deviceToken,
+          primaryActive: primaryActive,
+          onesignalRegistered: onesignalRegistered,
+          plexPass: plexPass,
+          dateFormat: dateFormat,
+          timeFormat: timeFormat,
+        );
+
+  NewServerModel copyWith({
+    int? id,
+    int? sortIndex,
+    String? plexName,
+    String? plexIdentifier,
+    String? tautulliId,
+    String? primaryConnectionAddress,
+    String? primaryConnectionProtocol,
+    String? primaryConnectionDomain,
+    String? primaryConnectionPath,
+    String? secondaryConnectionAddress,
+    String? secondaryConnectionProtocol,
+    String? secondaryConnectionDomain,
+    String? secondaryConnectionPath,
+    String? deviceToken,
+    bool? primaryActive,
+    bool? onesignalRegistered,
+    bool? plexPass,
+    String? dateFormat,
+    String? timeFormat,
+  }) {
+    return NewServerModel(
+      id: id ?? this.id,
+      sortIndex: sortIndex ?? this.sortIndex,
+      plexName: plexName ?? this.plexName,
+      plexIdentifier: plexIdentifier ?? this.plexIdentifier,
+      tautulliId: tautulliId ?? this.tautulliId,
+      primaryConnectionAddress:
+          primaryConnectionAddress ?? this.primaryConnectionAddress,
+      primaryConnectionProtocol:
+          primaryConnectionProtocol ?? this.primaryConnectionProtocol,
+      primaryConnectionDomain:
+          primaryConnectionDomain ?? this.primaryConnectionDomain,
+      primaryConnectionPath:
+          primaryConnectionPath ?? this.primaryConnectionPath,
+      secondaryConnectionAddress:
+          secondaryConnectionAddress ?? this.secondaryConnectionAddress,
+      secondaryConnectionProtocol:
+          secondaryConnectionProtocol ?? this.secondaryConnectionProtocol,
+      secondaryConnectionDomain:
+          secondaryConnectionDomain ?? this.secondaryConnectionDomain,
+      secondaryConnectionPath:
+          secondaryConnectionPath ?? this.secondaryConnectionPath,
+      deviceToken: deviceToken ?? this.deviceToken,
+      primaryActive: primaryActive ?? this.primaryActive,
+      onesignalRegistered: onesignalRegistered ?? this.onesignalRegistered,
+      plexPass: plexPass ?? this.plexPass,
+      dateFormat: dateFormat ?? this.dateFormat,
+      timeFormat: timeFormat ?? this.timeFormat,
+    );
+  }
+
+  // Create Settings from JSON data
+  factory NewServerModel.fromJson(Map<String, dynamic> json) {
+    bool primaryActiveBool = toBoolean(json['primary_active'].toString());
+    bool onesignalRegisteredBool =
+        toBoolean(json['onesignal_registered'].toString());
+    bool plexPass = toBoolean(json['plex_plexpass'].toString());
+
+    return NewServerModel(
+      id: json['id'],
+      sortIndex: json['sort_index'],
+      plexName: json['plex_name'],
+      plexIdentifier: json['plex_identifier'],
+      tautulliId: json['tautulli_id'],
+      primaryConnectionAddress: json['primary_connection_address'],
+      primaryConnectionProtocol: json['primary_connection_protocol'],
+      primaryConnectionDomain: json['primary_connection_domain'],
+      primaryConnectionPath: json['primary_connection_path'],
+      secondaryConnectionAddress: json['secondary_connection_address'],
+      secondaryConnectionProtocol: json['secondary_connection_protocol'],
+      secondaryConnectionDomain: json['secondary_connection_domain'],
+      secondaryConnectionPath: json['secondary_connection_path'],
+      deviceToken: json['device_token'],
+      primaryActive: primaryActiveBool,
+      onesignalRegistered: onesignalRegisteredBool,
+      plexPass: plexPass,
+      dateFormat: json['date_format'],
+      timeFormat: json['time_format'],
+    );
+  }
+
+  // Convert Settings to JSON to make it easier when we store it in the database
+  Map<String, dynamic> toJson() {
+    int? primaryActiveInt;
+    int? onesignalRegisteredInt;
+    int? plexPassInt;
+
+    switch (primaryActive) {
+      case (false):
+        primaryActiveInt = 0;
+        break;
+      case (true):
+        primaryActiveInt = 1;
+        break;
+    }
+
+    switch (onesignalRegistered) {
+      case (false):
+        onesignalRegisteredInt = 0;
+        break;
+      case (true):
+        onesignalRegisteredInt = 1;
+        break;
+    }
+
+    switch (plexPass) {
+      case (false):
+        plexPassInt = 0;
+        break;
+      case (true):
+        plexPassInt = 1;
+        break;
+    }
+
+    return {
+      'id': id,
+      'sort_index': sortIndex,
+      'plex_name': plexName,
+      'plex_identifier': plexIdentifier,
+      'tautulli_id': tautulliId,
+      'primary_connection_address': primaryConnectionAddress,
+      'primary_connection_protocol': primaryConnectionProtocol,
+      'primary_connection_domain': primaryConnectionDomain,
+      'primary_connection_path': primaryConnectionPath,
+      'secondary_connection_address': secondaryConnectionAddress,
+      'secondary_connection_protocol': secondaryConnectionProtocol,
+      'secondary_connection_domain': secondaryConnectionDomain,
+      'secondary_connection_path': secondaryConnectionPath,
+      'device_token': deviceToken,
+      'primary_active': primaryActiveInt,
+      'onesignal_registered': onesignalRegisteredInt,
+      'plex_pass': plexPassInt,
+      'date_format': dateFormat,
+      'time_format': timeFormat,
+    };
+  }
+}
---
+++ lib/core_new/database/domain/entities/new_server.dart
@@ -0,0 +1,68 @@
+import 'package:equatable/equatable.dart';
+
+class NewServer extends Equatable {
+  final int? id;
+  final int? sortIndex;
+  final String plexName;
+  final String plexIdentifier;
+  final String tautulliId;
+  final String primaryConnectionAddress;
+  final String primaryConnectionProtocol;
+  final String primaryConnectionDomain;
+  final String? primaryConnectionPath;
+  final String? secondaryConnectionAddress;
+  final String? secondaryConnectionProtocol;
+  final String? secondaryConnectionDomain;
+  final String? secondaryConnectionPath;
+  final String deviceToken;
+  final bool primaryActive;
+  final bool onesignalRegistered;
+  final bool plexPass;
+  final String? dateFormat;
+  final String? timeFormat;
+
+  NewServer({
+    this.id,
+    this.sortIndex,
+    required this.plexName,
+    required this.plexIdentifier,
+    required this.tautulliId,
+    required this.primaryConnectionAddress,
+    required this.primaryConnectionProtocol,
+    required this.primaryConnectionDomain,
+    this.primaryConnectionPath,
+    this.secondaryConnectionAddress,
+    this.secondaryConnectionProtocol,
+    this.secondaryConnectionDomain,
+    this.secondaryConnectionPath,
+    required this.deviceToken,
+    required this.primaryActive,
+    required this.onesignalRegistered,
+    required this.plexPass,
+    this.dateFormat,
+    this.timeFormat,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        sortIndex,
+        plexName,
+        plexIdentifier,
+        tautulliId,
+        primaryConnectionAddress,
+        primaryConnectionProtocol,
+        primaryConnectionDomain,
+        primaryConnectionPath,
+        secondaryConnectionAddress,
+        secondaryConnectionProtocol,
+        secondaryConnectionDomain,
+        secondaryConnectionPath,
+        deviceToken,
+        primaryActive,
+        onesignalRegistered,
+        plexPass,
+        dateFormat,
+        timeFormat,
+      ];
+}
