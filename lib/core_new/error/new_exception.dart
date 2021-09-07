f58fde1158e2b607a40443666d8090f7903436b7
TheMeanCanEHdian <drivard100@gmail.com>
2021-09-06 19:51:14 -0500

    Add new exceptions.

--- lib/core_new/error/new_exception.dart
+++ lib/core_new/error/new_exception.dart
@@ -1,10 +1,16 @@
-/// Throw when a TSL/SSL certificate is expired
+/// Throw when a TSL/SSL certificate is expired.
 class CertificateExpiredException implements Exception {}
 
-/// Throw when a HandshakeException with CERTIFICATE_VERIFY_FAILED is thrown
+/// Throw when a HandshakeException with CERTIFICATE_VERIFY_FAILED is thrown.
 class CertificateVerificationException implements Exception {}
 
-/// Throw when the provided connection protocol is not `http` or `https`
+/// Throw when connection details are missing.
+class ConnectionDetailsException implements Exception {}
+
+/// Throw when there is an error initializing the database.
+class DatabaseInitException implements Exception {}
+
+/// Throw when the provided connection protocol is not `http` or `https`.
 class IncorrectConnectionProtocolException implements Exception {}
 
 /// Throw when a json.decode() fails.
@@ -12,6 +18,9 @@ class JsonDecodeException implements Exception {}
 
 /// Throw when a server provides an undesired response.
 class ServerException implements Exception {}
+
+/// Throw when a returned ServerModel is null
+class ServerNotFoundException implements Exception {}
 
 /// Throw when trying to add a Tautulli server does not meet the min version.
 class ServerVersionException implements Exception {}
